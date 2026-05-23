2f6764f6c0e776c403982cbf2e39bf69fe1b2768
vitorhgbds <vitor_hgbds@hotmail.com>
2026-05-23 15:33:41 -0300

    Refactor run.bat to comment out unused model configurations and add logging directory creation for rolling-10 models

--- slide/run.bat
+++ slide/run.bat
@@ -1,69 +1,27 @@
 
 
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-5/forest.yaml --db-name optuna_studies_size.db
 
-REM ----------------- Raw Data -----------------
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-5/logistic-regression.yaml --db-name REM optuna_studies_size.db
 
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/raw/decision-tree.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/raw/forest.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/raw/logistic-regression.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/raw/xgboost.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/raw/lstm.yaml --db-name optuna_studies_size.db
-
-
-
-REM ----------------- Clean Data -----------------
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/clean/decision-tree.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/clean/forest.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/clean/logistic-regression.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/clean/xgboost.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/clean/lstm.yaml --db-name optuna_studies_size.db
-
-
-
-REM ----------------- Presence Data -----------------
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/presence/decision-tree.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/presence/forest.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/presence/logistic-regression.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/presence/xgboost.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/presence/lstm.yaml --db-name optuna_studies_size.db
-
-
-REM ----------------- Rolling 5 Data -----------------
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-5/decision-tree.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-5/forest.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-5/logistic-regression.yaml --db-name optuna_studies_size.db
-
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-5/xgboost.yaml --db-name optuna_studies_size.db
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-5/xgboost.yaml --db-name optuna_studies_size.db
 
 
 REM ----------------- Rolling 10 Data -----------------
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-10/decision-tree.yaml --db-name optuna_studies_size.db
+REM mkdir -p logs/rolling-10
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-10/decision-tree.yaml --db-name optuna_studies_size.db
 
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-10/forest.yaml --db-name optuna_studies_size.db
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-10/forest.yaml --db-name optuna_studies_size.db
 
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-10/logistic-regression.yaml --db-name optuna_studies_size.db
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-10/logistic-regression.yaml --db-name REM optuna_studies_size.db
 
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-10/xgboost.yaml --db-name optuna_studies_size.db
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-10/xgboost.yaml --db-name optuna_studies_size.db
 
 
 
 REM ----------------- Problematic BILSTM models -----------------
+python .\run.py --log-level DEBUG --model-config models/configs/final-models/presence/lstm.yaml --db-name optuna_studies_size.db
+
 python .\run.py --log-level DEBUG --model-config models/configs/final-models/raw/bilstm.yaml --db-name optuna_studies_size.db
 
 python .\run.py --log-level DEBUG --model-config models/configs/final-models/clean/bilstm.yaml --db-name optuna_studies_size.db
@@ -73,12 +31,12 @@ python .\run.py --log-level DEBUG --model-config models/configs/final-models/pre
 
 REM ----------------- Problematic KNN models -----------------
 
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/raw/knn.yaml --db-name optuna_studies_size.db
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/raw/knn.yaml --db-name optuna_studies_size.db
 
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/clean/knn.yaml --db-name optuna_studies_size.db
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/clean/knn.yaml --db-name optuna_studies_size.db
 
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/presence/knn.yaml --db-name optuna_studies_size.db
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/presence/knn.yaml --db-name optuna_studies_size.db
 
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-5/knn.yaml --db-name optuna_studies_size.db
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-5/knn.yaml --db-name optuna_studies_size.db
 
-python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-10/knn.yaml --db-name optuna_studies_size.db
+REM python .\run.py --log-level DEBUG --model-config models/configs/final-models/rolling-10/knn.yaml --db-name optuna_studies_size.db
